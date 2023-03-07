b3395d1d9aea8d8233960fa8048cf2c343393a95
Larry <larry.costigan5@gmail.com>
2023-03-07 03:47:58 +0000

    Move logo to root folder

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    mode: process.env.NODE_ENV || 'none',
+    mode: process.env.NODE_ENV || 'development',
     entry: "./src/index.js",
     output: {
         filename: "main.js",
